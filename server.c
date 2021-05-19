50cf0706a156d1e5c94187cb5f02a3b867a5d489
ZackHolby <zackholbrook44@gmail.com>
2021-05-19 12:01:30 -0400

    refactor code

--- server.c
+++ server.c
@@ -7,6 +7,7 @@ int main(){
 
     struct sockaddr_in sockAddr;
     const int PORT=8080;
+    char *str = "Hi from server";
 
     int server_fd = socket(AF_INET,SOCK_STREAM, 0);
     if(server_fd < 0){
@@ -16,10 +17,12 @@ int main(){
     fprintf(stdout, "Socket Created!");
 
 
-    memset((char *) &sockAddr, 0, sizeof(sockAddr));
+    
     sockAddr.sin_family = AF_INET;
     sockAddr.sin_addr.s_addr = htonl(INADDR_ANY);
     sockAddr.sin_port= htons(PORT);
+
+    memset(sockAddr.sin_zero, "\0", sizeof(sockAddr.sin_zero));
 
     int bindVal = bind(server_fd, (struct sockaddr *)&sockAddr, sizeof(sockAddr));
 
@@ -31,35 +34,39 @@ int main(){
 
     
 
-    int listenVal = listen(server_fd, 3)
+    int listenVal = listen(server_fd, 10);
 
     if(listenVal < 0){
         perror("Socket Listening failed");
         return 0;
     }
 
-    int new_socket = accept(server_fd, (struct sockaddr *)&sockAddr, (socklen_t*)&addrlen)
-    if(new_socket < 0){
-        perror("Socket Accept failed");
-        return 0;
+    while(1){
+        fprintf(stdout, "\n---------Waiting for connection----------\n");
+        int new_socket = accept(server_fd, (struct sockaddr *)&sockAddr, (socklen_t*)&addrlen)
+        if(new_socket < 0){
+            perror("Socket Accept failed");
+            return 0;
+        }
+        fprintf(stdout, "\n\nServer Listening...!\n");
+
+
+
+        char bufferMesg[25000] = {0};
+        int mesgIn = read(new_socket, bufferMesg, 25000);
+        fprintf(stdout, "\n\nMessage: +%s\n", bufferMesg);
+
+        if(mesgIn < 0){
+            fprintf(stdout, "No bytes read in");
+        }
+
+    
+        write(new_socket, str, strlen(str));
+        fprintf(stdout, "\n---------Message Sent----------\n");
+        close(new_socket);
+
     }
-    fprintf(stdout, "\n\nServer Listening...!\n");
 
-
-
-    char bufferMesg[1024] = {0};
-    int mesgIn = read(new_socket, bufferMesg, 1024);
-    fprintf(stdout, "\n\nMessage: +%s\n", bufferMesg);
-
-    if(mesgIn < 0){
-        fprintf(stdout, "No bytes read in");
-    }
-
-    char *str = "Hi from server";
-    write(new_socket, str, strlen(str));
-
-
-    close(new_socket);
 
 }
 
